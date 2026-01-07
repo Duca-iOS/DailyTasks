010f45d84adfb873f56afed89ded2dad34f2e6ca
Duca-iOS <ducaiphone14@gmail.com>
2026-01-07 14:21:46 +0100

    Created xCode project

---
+++ DailyTasks/DailyTasks/DailyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTasksApp.swift
+//  DailyTasks
+//
+//  Created by Dusan Mitrasinovic on 7. 1. 2026..
+//
+
+import SwiftUI
+
+@main
+struct DailyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
